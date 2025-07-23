355f3bed4ea4122d2568bd8d79c7da1e315de376
Alex Goode <alexgoodestudio@gmail.com>
2025-07-23 15:30:54 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-dark text-white">
+      <h1 className='display-1'>GSAP practice </h1>
+    </div>
+  );
+}
+
+export default App;
