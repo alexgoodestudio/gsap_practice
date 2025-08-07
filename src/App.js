d49f4971091899a7bd126e592b9160be6ac45a64
Alex Goode <alexgoodestudio@gmail.com>
2025-08-07 12:41:31 -0400

    new gsap guide

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Dashboard from "./Components/Dashboard";
-import ScrollBoxes from "./Components/ScrollBoxes";
-import TimelineBox from "./Components/TimelineBox";
-import RotateYBox from "./Components/RotateYBox";
-import SplitEntranceText from "./Components/SplitEntranceText";
-import MotionPath from "./Components/MotionPath";
-import XRandomize from "./Components/XRandomize";
-import ScrollPlayground from "./Components/ScrollPlayground";
+import ScrollTriggerToggleActions from "./Components/ScollTriggerToggleActions";
+
 
 function App() {
   return (
@@ -16,13 +11,8 @@ function App() {
       <div className="">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/1" element={<ScrollBoxes />} />
-          <Route path="/2" element={<TimelineBox />} />
-          <Route path="/3" element={<RotateYBox />} />
-          <Route path="/4" element={<SplitEntranceText />} />
-          <Route path="/5" element={<MotionPath />}/>
-          <Route path="/6" element={<XRandomize />}/>
-          <Route path="/7" element={<ScrollPlayground/>}/>
+          <Route path="/1" element={<ScrollTriggerToggleActions />} />
+
         </Routes>
       </div>
     </Router>
--- src/Nav.js
+++ src/Nav.js
@@ -5,13 +5,8 @@ function Nav() {
   return (
     <nav className="bg-black text-white px-6 py-4 flex gap-6">
       <Link to="/">Home</Link>
-      <Link to="/1">Scroll Boxes</Link>
-      <Link to="/2">Timeline Boxes</Link>
-      <Link to="/3">Rotate Y Box</Link>
-      <Link to="/4">Split Entrance Text</Link>
-      <Link to="/5">Motion Path</Link>
-      <Link to="/6">X_Randomize</Link>
-      <Link to="/7">Scroll Playground</Link>
+      <Link to="/1">Scroll Trigger Toggle Actions</Link>
+
     </nav>
   );
 }
